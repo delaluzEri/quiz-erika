080fde74030f3a2bc61559ff817632b73781878a
Erika De La Luz <erika.luz.c@gmail.com>
2015-08-15 22:37:26 -0500

    Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,6 +1,15 @@
 /**
  * Created by Erika on 15/08/2015.
  */
+// MW de autorización de accesos HTTP restringidos
+exports.loginRequired = function(req, res, next){
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+};
+
 // Get /login   -- Formulario de login
 exports.new = function(req, res) {
     var errors = req.session.errors || {};
--- routes/index.js
+++ routes/index.js
@@ -10,22 +10,22 @@ router.get('/', function(req, res) {
 });
 
 //Autoload de comandos con :quizId
-router.param('quizId', quizController.load); //autolad :quizId
+router.param('quizId',                      quizController.load); //autolad :quizId
 
 // Definición de rutas de sesion
-router.get('/login',  sessionController.new);     // formulario login
-router.post('/login', sessionController.create);  // crear sesión
-router.get('/logout', sessionController.destroy); // destruir sesión
+router.get('/login',                        sessionController.new);     // formulario login
+router.post('/login',                       sessionController.create);  // crear sesión
+router.get('/logout',                       sessionController.destroy); // destruir sesión
 
 //Definición de rutas de /quizes
 router.get('/quizes',                       quizController.index);
 router.get('/quizes/:quizId(\\d+)',         quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
-router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit',    quizController.edit);
-router.put('/quizes/:quizId(\\d+)',         quizController.update);
-router.delete('/quizes/:quizId(\\d+)',       quizController.destroy);
+router.get('/quizes/new',                   sessionController.loginRequired, quizController.new);
+router.post('/quizes/create',               sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',    sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',         sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)',      sessionController.loginRequired, quizController.destroy);
 
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',    commentController.new);
